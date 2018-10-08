714986e6e1ed8b1e2504cee04161d8a92094434d
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-10-08 12:42:58 +0200

    Added playground for multi-threading tests

---
+++ SoundExporterPlayground/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using SoundExporter;
+
+namespace SoundExporterPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var inputFiles = Directory.EnumerateFiles(currentDirectory, "*.wav", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in inputFiles)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file);
+                var outputFileName = $"{fileName}-out.wav";
+                var outputPath = Path.Join(currentDirectory, outputFileName);
+                var thread = new Thread(() =>
+                {
+                    Debug.WriteLine($"{fileName} - Converting...");
+
+                    try
+                    {
+                        WavConverter.Convert(file, outputPath, SampleRate.Low, ChannelFormat.Mono, BitRate.Low);
+                    }
+                    catch (InvalidDataException)
+                    {
+                        Debug.WriteLine($"{fileName} - Ops, unsupported format");
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.WriteLine($"{fileName} - Ops, {exception.Message}");
+                    }
+                    finally
+                    {
+                        Debug.WriteLine($"{fileName} - Done!");
+                    }
+                });
+                thread.Start();
+            }
+        }
+    }
+}
